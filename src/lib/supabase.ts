1e77473e5ec1110e5e89c1d9f781255c4f4f0740
Enes Yurtsever <slothful_@outlook.com>
2025-12-07 16:14:11 +0300

    fix

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,11 +1,10 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = ""; //process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey =
-  ""; //process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 if (!supabaseUrl || !supabaseKey) {
-  throw new Error("Supabase environment variables are not set");
+  throw new Error('Supabase environment variables are not set');
 }
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
